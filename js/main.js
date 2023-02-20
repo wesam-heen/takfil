8780b1f06fbaf1c18f2098545462d0d361b0ffe1
wesame <heenwesam@gmail.com>
2023-02-20 15:35:47 +0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+
+
+//for menu bar transition
+const menu=document.querySelector(".menu-icon")
+const navBar=document.querySelector(".nav-bar")
+const menuSpan=document.querySelectorAll('.menu-icon span')
+
+
+menu.addEventListener('click',()=>{
+    navBar.classList.toggle('nav-bar-hide')
+    menu.classList.toggle('open')
+
+})
+
---
+++ php/cs1-process.php
@@ -0,0 +1,32 @@
+<?php
+$name = $_POST["cs1Name"];
+$email = $_POST["cs1Email"];
+$phone_number = $_POST["cs1PhoneNum"];
+ 
+$EmailTo = "moradxd@gmail.com";
+$Subject = "Message from " . $name;
+ 
+// prepare email body text
+$Body = "Name: ";
+$Body .= $name;
+$Body .= "\n";
+ 
+$Body .= "Email: ";
+$Body .= $email;
+$Body .= "\n";
+ 
+$Body .= "Phone Number: ";
+$Body .= $phone_number;
+$Body .= "\n";
+  
+// send email
+$success = mail($EmailTo, $Subject, $Body, "From:".$email);
+ 
+// redirect to success page
+if ($success){
+   echo "success";
+}else{
+    echo "invalid";
+}
+ 
+?>
