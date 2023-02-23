92f2ab69a9731baa96501536159c139d545fdda9
wesame <heenwesam@gmail.com>
2023-02-23 10:45:15 +0400

    upadte

--- config.php
+++ config.php
@@ -24,13 +24,45 @@ $message = $_POST['message'];
 // Insert the data into the SQL table
 $sql = "INSERT INTO contact (name, email, phone, message) VALUES ('$name', '$email', '$phone', '$message')";
 if (mysqli_query($conn, $sql)) {
-  echo "Record inserted successfully";
+  echo "we will contact";
 } else {
   echo "Error inserting record: " . mysqli_error($conn);
 }
 
+
+$EmailTo = "contact@takfil.net";
+$Subject = "Message from " . $name;
+ 
+// prepare email body text
+$Body = "Name: ";
+$Body .= $name;
+$Body .= "\n";
+ 
+$Body .= "Email: ";
+$Body .= $email;
+$Body .= "\n";
+ 
+$Body .= "Phone Number: ";
+$Body .= $phone;
+$Body .= "\n";
+  
+  
+$Body .= "Message : ";
+$Body .= $message;
+$Body .= "\n";  
+  
+  
+  
+// send email
+$success = mail($EmailTo, $Subject, $Body, "From:".$email, );
+
 // Close the database connection
 mysqli_close($conn);
+
+
+
+header('Location: /');
+
 ?>
 
 
--- php/cs1-process.php
+++ php/cs1-process.php
@@ -1,32 +1,61 @@
 <?php
-$name = $_POST["cs1Name"];
-$email = $_POST["cs1Email"];
-$phone_number = $_POST["cs1PhoneNum"];
+
  
-$EmailTo = "contact@takfil.net";
-$Subject = "Message from " . $name;
+// Create connection
  
-// prepare email body text
-$Body = "Name: ";
-$Body .= $name;
-$Body .= "\n";
- 
-$Body .= "Email: ";
-$Body .= $email;
-$Body .= "\n";
- 
-$Body .= "Phone Number: ";
-$Body .= $phone_number;
-$Body .= "\n";
-  
-// send email
-$success = mail($EmailTo, $Subject, $Body, "From:".$email);
- 
-// redirect to success page
-if ($success){
-   echo "success";
-}else{
-    echo "invalid";
+// Connect to the database
+$servername = "localhost";
+$database = "u284314170_takfildb";
+$username = "u284314170_takfil";
+$password = "Takfil/2023";
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+// Check for errors
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
 }
+
+// Get the form data
+
+$email = $_POST["cs1Email"];
+
+
+
+// Insert the data into the SQL table
+$sql = "INSERT INTO subscribers (email) VALUES ('$email')";
+
  
+
+
+// Close the database connection
+mysqli_close($conn);
+
+
+if (mysqli_query($conn, $sql)) {
+  echo "we will contact";
+} else {
+  echo "Error inserting record: " . mysqli_error($conn);
+}
+
+
+
+
+?>
+
+
+
+
+
+
+ 
+ 
+
+
+
+
+
+
+
+
+
 ?>
